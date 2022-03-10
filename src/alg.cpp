d9494c9d8a7195bf2aacb6152accb6865c3eec63
artem <99349934+ArtemPr1@users.noreply.github.com>
2022-03-10 23:46:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,23 @@
+
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-    int count = 0;
-  for (int i = 0; i < size; i++) {
-    if (value == arr[i])
-      count += 1;
+  int num = 0, mid = size/2;
+  if (mid < size) {
+    for (int i = mid; i < size; i++) {
+      if (arr[i] == value)
+        num++;
+    }
+  } else if (mid > size) {
+    for (int i = mid; i >= 0; i++) {
+      if (arr[i] == value)
+        num++;
+    }
+  } else if (mid == size) {
+    for (int i = 0; i < size; i++) {
+      if (arr[i] == value)
+        num++;
+    }
   }
-    return count;
-    else
-
-  return 0; // если ничего не найдено
+  return num;
 }
